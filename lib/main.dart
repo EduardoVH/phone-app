7b6e90705c54e57db457e70c153ff950d576bac4
Jose Angel Ortega Merlin <angelortegamerlin@gmail.com>
2024-09-09 19:22:12 -0600

    add number Galvez

--- lib/main.dart
+++ lib/main.dart
@@ -132,7 +132,7 @@ class StudentTableScreen extends StatelessWidget {
                 ],
                 rows: [
                   _buildDataRow('Darinel', '9613021060', '221192'),
-                  _buildDataRow('Ulises', 'number', '213691'),
+                  _buildDataRow('Ulises', '9651032159', '213691'),
                   _buildDataRow('Merlin', '9515271070', '221255'),
                 ],
               ),
